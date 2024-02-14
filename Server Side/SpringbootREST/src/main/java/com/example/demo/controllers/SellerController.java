b4348beb87aabb77a635e124afe0eafca4b018e2
PravinWagh1 <149952913+PravinWagh1@users.noreply.github.com>
2024-02-14 08:42:10 +0530

    seller registration part

---
+++ Server Side/SpringbootREST/src/main/java/com/example/demo/controllers/SellerController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+public class SellerController {
+
+//	@Autowired
+//	SellerService sservice;
+	
+	
+}
