8cdba3dff38a09b560e7953e538a4906a42a5fe9
PravinWagh1 <149952913+PravinWagh1@users.noreply.github.com>
2024-02-15 00:09:14 +0530

    Consumer Registration Springboot side part

--- Server Side/SpringbootREST/src/main/java/com/example/demo/services/ConsumerService.java
+++ Server Side/SpringbootREST/src/main/java/com/example/demo/services/ConsumerService.java
@@ -1,5 +1,22 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Consumer;
+import com.example.demo.repositories.ConsumerRepository;
+
+@Service
 public class ConsumerService {
 
+
+
+	@Autowired
+	ConsumerRepository crepo;
+	
+	public Consumer saveConsumer(Consumer consumer)
+	{
+		return crepo.save(consumer);
+		
+	}
 }
